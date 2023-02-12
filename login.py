35ad6b200b6762ae1cd515c7f0d1adeda73ee4e0
tobit bushenyula <mrbushenyula@gmail.com>
2023-02-11 16:28:42 -0800

    EmailLoginValidation
    this is basically a program that ask the user his details in  order to access his/her email account. here we are only asking for a valid email and a valid password

---
+++ login.py
@@ -0,0 +1,19 @@
+#QUESTION 2:
+
+def login():
+   
+   gmail="martha@gmail.com"
+   password="martha"
+   passwords=""
+   
+   account=str(input("enter your gmail here: "))  
+   
+   while account!=gmail:
+      print("invalid account🤨")
+      account=str(input("enter your gmail here: "))
+   while account==gmail and passwords!=password:
+      print("invalid password😢")
+      passwords=str(input("enter your password here: "))
+   print("welcome to your account dear martha 😊")
+      
+login()
