7aa2500dc63d29da76828f1ad49cbd0c2af7fe50
tobit bushenyula <mrbushenyula@gmail.com>
2023-02-11 16:27:09 -0800

    StringLengthCalculator
    this is a simple program that can count the number of characters contained in a string

---
+++ length.py
@@ -0,0 +1,12 @@
+
+#QUESTION 1:
+#length of a string 
+def main():
+   
+   statement="i am tobit bushenyula, a student in computer science"
+   print_len(statement)
+   
+def print_len(lenght):
+   print(len(lenght))
+   
+main()
